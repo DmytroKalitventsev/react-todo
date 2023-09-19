9974f4f400e0dc6620008896f51f3d3e5ccae74b
Dmytro <dm.kalitventsev@gmail.com>
2023-09-19 12:07:39 +0300

    Created a local store for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './styles/common.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ToolsForTodos from './context/ToolsForTodos';
 import TodoCreator from './components/TodoCreator';
 import TodoLists from './components/TodoLists';
@@ -8,30 +8,49 @@ import TodoHeader from './components/TodoHeader';
 const App = () => {
 	const [dataTodos, setDataTodos] = useState([]);
 
+	const getLocalDataTodos = () => JSON.parse(localStorage.getItem('dataTodos'));
+	const saveLocalDataTodos = () => localStorage.setItem('dataTodos', JSON.stringify(dataTodos));
+
+	useEffect(() => {
+		if (getLocalDataTodos()) setDataTodos(getLocalDataTodos());
+	}, []);
+
+	useEffect(() => {
+		saveLocalDataTodos();
+	}, [dataTodos]);
+
 	const addTodo = title => {
 		const id = dataTodos.length ? dataTodos[dataTodos.length - 1].id + 1 : 1;
 
 		setDataTodos(
 			[...dataTodos, { id, title, completed: false, changing: false }]
 		)
+
+		saveLocalDataTodos();
 	}
 
 	const deleteTodo = id => {
 		setDataTodos(
 			dataTodos.filter(todo => (id !== todo.id) && { ...todo })
 		)
+
+		saveLocalDataTodos();
 	}
 
 	const changeTodo = (id, title) => {
 		setDataTodos(
 			dataTodos.map(todo => (id === todo.id) ? { ...todo, title, changing: !todo.changing } : { ...todo, changing: false })
 		)
+
+		saveLocalDataTodos();
 	}
 
 	const toggleCompleteTodo = (id) => {
 		setDataTodos(
 			dataTodos.map(todo => (id === todo.id) ? { ...todo, completed: !todo.completed } : todo)
 		)
+
+		saveLocalDataTodos();
 	}
 
 	const toolsForTodos = {
